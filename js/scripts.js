6b098d9bb932da00899299683319915af0886496
Reikou Dev <arthurgabrielsilvadacunha25@gmail.com>
2026-08-02 21:47:44 -0300

    atualização final.

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,77 @@ cards.forEach(card => {
     });
 
 });
-const musica = document.getElementById("musica");
-const btn = document.getElementById("btnMusica");
+document.addEventListener("DOMContentLoaded", () => {
 
-btn.addEventListener("click", () => {
-    musica.play()
-        .then(() => {
-            console.log("Música tocando!");
-        })
-        .catch((erro) => {
-            console.error("Erro:", erro);
-        });
+    const musica = document.getElementById("musica");
+    const botao = document.getElementById("playMusic");
+
+
+    botao.addEventListener("click", () => {
+
+        if (musica.paused) {
+
+            musica.play();
+            botao.innerHTML = "⏸️";
+
+        } else {
+
+            musica.pause();
+            botao.innerHTML = "🎵";
+
+        }
+
+    });
+
+});
+const sim = document.getElementById("sim");
+const nao = document.getElementById("nao");
+
+
+nao.addEventListener("mouseover",()=>{
+
+
+    nao.style.position="fixed";
+
+
+    nao.style.left =
+    Math.random()*80+"vw";
+
+
+    nao.style.top =
+    Math.random()*80+"vh";
+
+
+});
+
+
+
+sim.addEventListener("click",()=>{
+
+
+document.body.innerHTML = `
+
+<div class="pedido-container">
+
+
+<h1>
+Eu sabia que era você ❤️
+</h1>
+
+
+<div class="heart-final">
+💗
+</div>
+
+
+<h2>
+Obrigado por escolher viver essa história comigo.
+</h2>
+
+
+</div>
+
+`;
+
+
 });
